d35d35a0e1f80b917c2e259db16a56da16792d65
Jademalo <386846+Jademalo@users.noreply.github.com>
2019-09-11 06:19:30 +0100

    Folder organisation with debug for packaging the tgas

--- Standalone/ArtFrame.lua
+++ Standalone/ArtFrame.lua
@@ -1,1 +1,11 @@
 local JadeUI = juicTable
+
+
+--@debug@
+--Put file paths Interface/AddOns/JadeUI-Classic/Media/background/G13Classic/xxx.tga here
+--@end-debug@
+
+
+--[===[@non-debug@
+--Put file paths Interface/AddOns/JadeUI-Classic/Media/xxx.tga here
+--@end-non-debug@]===]
