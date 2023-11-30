733f97df989079406842b5e2766e5aa1ff1a3d50
Jademalo <386846+Jademalo@users.noreply.github.com>
2023-11-30 16:09:57 +0000

    Draw basic art frames

--- ArtFrame.lua
+++ ArtFrame.lua
@@ -1,11 +1,79 @@
-local JadeUI = juicTable
+--------------------------------------------
+--Variables
+--------------------------------------------
+local name, JadeUI = ...
 
-
+--------------------------------------------
+--Artwork
+--------------------------------------------
 --@debug@
---Put file paths Interface/AddOns/JadeUI-Classic/Media/background/G13Classic/xxx.tga here
+local g13MainBarTexture = "Interface/AddOns/JadeUI-Classic/Media/background/G13Classic/G13MainBar.tga"
+local g13MaxCoverTexture = "Interface/AddOns/JadeUI-Classic/Media/background/G13Classic/G13MaxCover.tga"
+local g13RepBarTexture = "Interface/AddOns/JadeUI-Classic/Media/background/G13Classic/G13RepBar.tga"
+local g13TopBarTexture = "Interface/AddOns/JadeUI-Classic/Media/background/G13Classic/G13TopBar.tga"
+local g13XPBarTexture = "Interface/AddOns/JadeUI-Classic/Media/background/G13Classic/G13XPBar.tga"
+local petBarTexture = "Interface/AddOns/JadeUI-Classic/Media/background/G13Classic/PetBar.tga"
 --@end-debug@
 
-
 --[===[@non-debug@
---Put file paths Interface/AddOns/JadeUI-Classic/Media/xxx.tga here
+local g13MainBarTexture = "Interface/AddOns/JadeUI-Classic/Media/G13MainBar.tga"
+local g13MaxCoverTexture = "Interface/AddOns/JadeUI-Classic/Media/G13MaxCover.tga"
+local g13RepBarTexture = "Interface/AddOns/JadeUI-Classic/Media/G13RepBar.tga"
+local g13TopBarTexture = "Interface/AddOns/JadeUI-Classic/Media/G13TopBar.tga"
+local g13XPBarTexture = "Interface/AddOns/JadeUI-Classic/Media/G13XPBar.tga"
+local petBarTexture = "Interface/AddOns/JadeUI-Classic/Media/PetBar.tga"
 --@end-non-debug@]===]
+
+local endstopTexture = "Interface/MAINMENUBAR/UI-MainMenuBar-EndCap-Dwarf"
+
+--------------------------------------------
+--Functions
+--------------------------------------------
+function JadeUI.createArtFrame()
+    local g13MainBar = JadeUIBar:CreateTexture()
+    g13MainBar:SetPoint("BOTTOM", JadeUIBar, "BOTTOM")
+    g13MainBar:SetTexture(g13MainBarTexture)
+    g13MainBar:SetDrawLayer("BACKGROUND", 0)
+
+    local g13TopBar = JadeUIBar:CreateTexture()
+    g13TopBar:SetPoint("BOTTOM", JadeUIBar, "BOTTOM", 0, 43)
+    g13TopBar:SetTexture(g13TopBarTexture)
+    g13TopBar:SetDrawLayer("BACKGROUND", 0)
+
+
+    local leftEndstop = JadeUIBar:CreateTexture()
+    leftEndstop:SetPoint("BOTTOMRIGHT", JadeUIBar, "BOTTOM", -268, 0)
+    leftEndstop:SetTexture(endstopTexture)
+    leftEndstop:SetDrawLayer("BACKGROUND", 1)
+
+    local rightEndstop = JadeUIBar:CreateTexture()
+    rightEndstop:SetPoint("BOTTOMLEFT", JadeUIBar, "BOTTOM", 268, 0)
+    rightEndstop:SetTexture(endstopTexture)
+    rightEndstop:SetTexCoord(1, 0, 0, 1) --Mirror Texture
+    rightEndstop:SetDrawLayer("BACKGROUND", 1)
+
+
+    local g13XPBar = JadeUIBar:CreateTexture()
+    g13XPBar:SetPoint("BOTTOM", JadeUIBar, "BOTTOM", 0, 43)
+    g13XPBar:SetTexture(g13XPBarTexture)
+    g13XPBar:SetDrawLayer("BACKGROUND", -3)
+
+    local g13RepBar = JadeUIBar:CreateTexture()
+    g13RepBar:SetPoint("BOTTOM", JadeUIBar, "BOTTOM", 0, 51)
+    g13RepBar:SetTexture(g13RepBarTexture)
+    g13RepBar:SetDrawLayer("BACKGROUND", -2)
+
+    local g13MaxCover = JadeUIBar:CreateTexture()
+    g13MaxCover:SetPoint("BOTTOM", JadeUIBar, "BOTTOM", 0, 43)
+    g13MaxCover:SetTexture(g13MaxCoverTexture)
+    g13MaxCover:SetDrawLayer("BACKGROUND", -1)
+
+
+    local petBar = JadeUIBar:CreateTexture()
+    petBar:SetParent(PetActionBarFrame)
+    petBar:SetPoint("LEFT", PetActionBarFrame, "LEFT", 0, 8.5)
+    petBar:SetTexture(petBarTexture)
+
+
+
+end
--- Core.lua
+++ Core.lua
@@ -1,4 +1,58 @@
--- Declare the addon namespace table as global so it can be accessed in the other files
-_, juicTable = ...
+--------------------------------------------
+--Check for project type
+--------------------------------------------
+local isVanilla = (LE_EXPANSION_LEVEL_CURRENT == LE_EXPANSION_CLASSIC)
+local isTBC = (LE_EXPANSION_LEVEL_CURRENT == LE_EXPANSION_BURNING_CRUSADE)
+local isWrath = (LE_EXPANSION_LEVEL_CURRENT == LE_EXPANSION_NORTHREND)
 
-local JadeUI = juicTable
+--------------------------------------------
+--Variables
+--------------------------------------------
+local addonName, JadeUI = ... --Discard the addon name and set the namespace table as a variable
+JadeUIBar = CreateFrame("Frame", "JadeUI Main Frame", UIParent)
+
+--------------------------------------------------------------------------------
+--Core Functions
+--------------------------------------------------------------------------------
+local function startupPrint() --Startup message
+    print ("~JadeUI~")
+    if isVanilla or isTBC or isWrath then
+        print("Classic WoW Detected")
+    end
+end
+
+--------------------------------------------------------------------------------
+--Event Registration
+--------------------------------------------------------------------------------
+JadeUIBar:RegisterEvent("ADDON_LOADED")
+JadeUIBar:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+
+--------------------------------------------------------------------------------
+--Event Handler
+--------------------------------------------------------------------------------
+JadeUIBar:SetScript("OnEvent", function(self, event, arg1, arg2)
+
+    if event == "ADDON_LOADED" and arg1 == addonName then
+        JadeUIDB = JadeUIDB or {} --Create a table for saved variables
+        JadeUIDB.blizzXPBar = (JadeUIDB.blizzXPBar or 0)
+        JadeUIDB.endstopType = (JadeUIDB.endstopType or 0)
+        JadeUIDB.mouseover = (JadeUIDB.mouseover or 0)
+        JadeUIDB.moveMinimap = (JadeUIDB.moveMinimap or 0)
+        JadeUIDB.minimapScale = (JadeUIDB.minimapScale or 0)
+        JadeUIDB.moveUnitFrames = (JadeUIDB.moveUnitFrames or 0)
+        JadeUIDB.stanceBarHide = (JadeUIDB.stanceBarHide or 0)
+        JadeUIDB.keyCover = (JadeUIDB.keyCover or 0)
+    end
+
+    if event == "PLAYER_ENTERING_WORLD" then
+        JadeUIBar:SetFrameStrata("BACKGROUND")
+        JadeUIBar:SetSize(745, 210)
+        JadeUIBar:SetPoint("BOTTOM", UIParent, "BOTTOM")
+        JadeUI.createArtFrame()
+        MainMenuBar:Hide()
+        --local width,height = GetPhysicalScreenSize()
+        --UIParent:SetScale((768/height)*2)
+    end
+
+end)
