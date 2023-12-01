38c72c41c9a612b539f72888d3a48c44a57fb44b
Jademalo <386846+Jademalo@users.noreply.github.com>
2023-12-01 16:31:31 +0000

    Final tidy up of options menu

--- Options.lua
+++ Options.lua
@@ -5,9 +5,26 @@ local addonName, JadeUI = ...
 
 local itemSpacing = -5 --The space between each entry in the list
 local optionsPanel = CreateFrame("Frame") --The main options panel frame
+optionsPanel.name = "JadeUI Classic" --CreateFrame call (CreateFrame("Frame", "Name")) puts "Name" in the global scope, so _G["Name"] will retrieve the panel. The panel.name is a field on the panel itself.
 
 --Register ADDON_LOADED to run the main code when the addon is loaded
 optionsPanel:RegisterEvent("ADDON_LOADED")
+
+--Initialise SavedVariables
+local function savedVariablesInit()
+    JadeUIDB = JadeUIDB or {} --Create a table for saved variables
+        JadeUIDB.showTalents = (JadeUIDB.showTalents or false) --0 is truthy, so only false or nil will result in the default being read.
+        JadeUIDB.moveUnitFrames = (JadeUIDB.moveUnitFrames or false)
+        JadeUIDB.moveMinimap = (JadeUIDB.moveMinimap or false)
+        JadeUIDB.endstopType = (JadeUIDB.endstopType or 1)
+        JadeUIDB.pixelScale = (JadeUIDB.pixelScale or false)
+
+        JadeUIDB.blizzXPBar = (JadeUIDB.blizzXPBar or 0)
+        JadeUIDB.mouseover = (JadeUIDB.mouseover or 0)
+        JadeUIDB.minimapScale = (JadeUIDB.minimapScale or 0)
+        JadeUIDB.stanceBarHide = (JadeUIDB.stanceBarHide or 0)
+        JadeUIDB.keyCover = (JadeUIDB.keyCover or 0)
+end
 
 --------------------------------------------
 --Functions
@@ -62,126 +79,121 @@ local function createCheckbox(name, description, savedVariable)
 end
 
 
+--------------------------------------------
+--Left Column Menu Entries
+--------------------------------------------        
+local function buildLeftColumn()
+    --Title
+    local title = optionsPanel:CreateFontString("ARTWORK", nil, "GameFontNormalLarge")
+    title:SetPoint("TOP")
+    title:SetText("JadeUI Classic")
 
+    --Checkbox for showing the talent button
+    local talentCheckbox = createCheckbox(
+        "Show Talent Button",
+        "This option will display the talent button in the Menu Bar regardless of player level",
+        JadeUIDB.showTalents
+    )
+    talentCheckbox:HookScript("OnClick", function(_, btn, down)
+        JadeUIDB.showTalents = talentCheckbox:GetChecked()
+        UpdateMicroButtons()
+    end)
+
+    --Checkbox for moving the unitframes
+    local unitFramesCheckbox = createCheckbox(
+        "Move Unitframes",
+        "Move the player unitframes down to the bottom centre of the screen\nReload required to disable",
+        JadeUIDB.moveUnitFrames
+    )
+    unitFramesCheckbox:HookScript("OnClick", function(_, btn, down)
+        JadeUIDB.moveUnitFrames = unitFramesCheckbox:GetChecked()
+        if not unitFramesCheckbox:GetChecked() then
+            C_UI.Reload()
+        else
+            JadeUI.blizzUIMove()
+        end
+    end)
+
+    --Checkbox for moving the Minimap
+    local minimapcheckbox = createCheckbox(
+        "Move Minimap",
+        "Move the Minimap down to the bottom right corner of the screen\nReload required to disable",
+        JadeUIDB.moveMinimap
+    )
+    minimapcheckbox:HookScript("OnClick", function(_, btn, down)
+        JadeUIDB.moveMinimap = minimapcheckbox:GetChecked()
+        if not minimapcheckbox:GetChecked() then
+            C_UI.Reload()
+        else
+            JadeUI.blizzUIMove()
+        end
+    end)
+
+    --Checkbox for forcing a specific UI Scale
+    local uiScaleCheckbox = createCheckbox(
+        "1:1 UI Scale",
+        "Set the UI scaling so that elements display at a 1:1 pixel ratio",
+        JadeUIDB.pixelScale
+    )
+    uiScaleCheckbox:HookScript("OnClick", function(_, btn, down)
+        JadeUIDB.pixelScale = uiScaleCheckbox:GetChecked()
+        JadeUI.SetScale()
+    end)
+end
+
+
+--------------------------------------------
+--Right Column Menu Entries
+--------------------------------------------  
+local function buildRightColumn()
+    --Create a dropdown to manage Endstops
+    local endstopLabel = optionsPanel:CreateFontString(nil, 'ARTWORK', 'GameFontHighlightSmall')
+    appendOptionRight(endstopLabel)
+    endstopLabel:SetText('Select endstop artwork:')
+
+
+    local endstopDropDown = CreateFrame("Frame", "Endstop Menu", optionsPanel, "UIDropDownMenuTemplate")
+    addTooltip(endstopDropDown) --Add a tooltip to the dropdown
+
+    local function endstopDropDownHandler(self, arg1, arg2, checked) --Arguments have to be (self, arg1, arg2, checked), self being info from UIDropDownMenu_CreateInfo (self.arg1 and arg1 are the same)  - https://www.townlong-yak.com/framexml/latest/UIDropDownMenu.lua#276
+        JadeUI.setEndstop(self.value)
+        JadeUIDB.endstopType = self.value
+        UIDropDownMenu_SetSelectedValue(endstopDropDown,self.value) --This sets the label after selecting an option
+    end
+
+    local function endstopDropDownInitialise(frame, level, menulist) --The initialisation function for the dropdown
+        local endstopTypes = { "None", "Gryphon", "Lion" }
+        for i, type in next, endstopTypes do
+            local info = UIDropDownMenu_CreateInfo()
+            info.text = type
+            info.value = i-1
+            info.func = endstopDropDownHandler
+            UIDropDownMenu_AddButton(info)
+        end
+        UIDropDownMenu_SetSelectedValue(endstopDropDown, JadeUIDB.endstopType) --This sets the label initially based on the SavedVariable
+    end
+
+    endstopDropDown:SetPoint("TOPLEFT", endstopLabel, "BOTTOMLEFT", -15, -5)
+    table.insert(JadeUI.OptionsListRight, endstopDropDown) --This frame has it's relative point set manually since it needs to be closer, and is added manually
+    UIDropDownMenu_SetWidth(endstopDropDown, 100)
+    endstopDropDown.tooltipText = "Select Endstop Artwork"
+    endstopDropDown.tooltipRequirement = "Select the artwork to be displayed for the endstops on the main bar"
+    UIDropDownMenu_Initialize(endstopDropDown, endstopDropDownInitialise)
+end
+
+
+--------------------------------------------------------------------------------
+--Event Handler
+--------------------------------------------------------------------------------
 optionsPanel:SetScript("OnEvent", function(self, event, arg1, arg2)
 
     if event == "ADDON_LOADED" and arg1 == addonName then
 
-        --Initialise SavedVariables
-        JadeUIDB = JadeUIDB or {} --Create a table for saved variables
-            JadeUIDB.showTalents = (JadeUIDB.showTalents or false) --0 is truthy, so only false or nil will result in the default being read.
-            JadeUIDB.moveUnitFrames = (JadeUIDB.moveUnitFrames or false)
-            JadeUIDB.moveMinimap = (JadeUIDB.moveMinimap or false)
-            JadeUIDB.endstopType = (JadeUIDB.endstopType or 1)
-            JadeUIDB.pixelScale = (JadeUIDB.pixelScale or false)
+        savedVariablesInit()
+        buildLeftColumn()
+        buildRightColumn()
 
-            JadeUIDB.blizzXPBar = (JadeUIDB.blizzXPBar or 0)
-            JadeUIDB.mouseover = (JadeUIDB.mouseover or 0)
-            JadeUIDB.minimapScale = (JadeUIDB.minimapScale or 0)
-            JadeUIDB.stanceBarHide = (JadeUIDB.stanceBarHide or 0)
-            JadeUIDB.keyCover = (JadeUIDB.keyCover or 0)
-
-        --------------------------------------------
-        --Left Column Menu Entries
-        --------------------------------------------        
-        --Title
-        local title = optionsPanel:CreateFontString("ARTWORK", nil, "GameFontNormalLarge")
-        title:SetPoint("TOP")
-        title:SetText("JadeUI Classic")
-
-        --Checkbox for showing the talent button
-        local talentCheckbox = createCheckbox(
-            "Show Talent Button",
-            "This option will display the talent button in the Menu Bar regardless of player level",
-            JadeUIDB.showTalents
-        )
-        talentCheckbox:HookScript("OnClick", function(_, btn, down)
-            JadeUIDB.showTalents = talentCheckbox:GetChecked()
-            UpdateMicroButtons()
-        end)
-
-        --Checkbox for moving the unitframes
-        local unitFramesCheckbox = createCheckbox(
-            "Move Unitframes",
-            "Move the player unitframes down to the bottom centre of the screen\nReload required to disable",
-            JadeUIDB.moveUnitFrames
-        )
-        unitFramesCheckbox:HookScript("OnClick", function(_, btn, down)
-            JadeUIDB.moveUnitFrames = unitFramesCheckbox:GetChecked()
-            if not unitFramesCheckbox:GetChecked() then
-                C_UI.Reload()
-            else
-                JadeUI.blizzUIMove()
-            end
-        end)
-
-        --Checkbox for moving the Minimap
-        local minimapcheckbox = createCheckbox(
-            "Move Minimap",
-            "Move the Minimap down to the bottom right corner of the screen\nReload required to disable",
-            JadeUIDB.moveMinimap
-        )
-        minimapcheckbox:HookScript("OnClick", function(_, btn, down)
-            JadeUIDB.moveMinimap = minimapcheckbox:GetChecked()
-            if not minimapcheckbox:GetChecked() then
-                C_UI.Reload()
-            else
-                JadeUI.blizzUIMove()
-            end
-        end)
-
-        --Checkbox for forcing a specific UI Scale
-        local uiScaleCheckbox = createCheckbox(
-            "1:1 UI Scale",
-            "Set the UI scaling so that elements display at a 1:1 pixel ratio",
-            JadeUIDB.pixelScale
-        )
-        uiScaleCheckbox:HookScript("OnClick", function(_, btn, down)
-            JadeUIDB.pixelScale = uiScaleCheckbox:GetChecked()
-            JadeUI.SetScale()
-        end)
-
-
-        --------------------------------------------
-        --Right Column Menu Entries
-        --------------------------------------------  
-        --Create a dropdown to manage Endstops
-        local endstopLabel = optionsPanel:CreateFontString(nil, 'ARTWORK', 'GameFontHighlightSmall')
-        appendOptionRight(endstopLabel)
-        endstopLabel:SetText('Select endstop artwork:')
-        
-
-        local endstopDropDown = CreateFrame("Frame", "Endstop Menu", optionsPanel, "UIDropDownMenuTemplate")
-        addTooltip(endstopDropDown) --Add a tooltip to the dropdown
-
-        local function endstopDropDownHandler(self, arg1, arg2, checked) --Arguments have to be (self, arg1, arg2, checked), self being info from UIDropDownMenu_CreateInfo (self.arg1 and arg1 are the same)  - https://www.townlong-yak.com/framexml/latest/UIDropDownMenu.lua#276
-            JadeUI.setEndstop(self.value)
-            JadeUIDB.endstopType = self.value
-            UIDropDownMenu_SetSelectedValue(endstopDropDown,self.value) --This sets the label after selecting an option
-        end
-
-        local function endstopDropDownInitialise(frame, level, menulist) --The initialisation function for the dropdown
-            local endstopTypes = { "None", "Gryphon", "Lion" }
-            for i, type in next, endstopTypes do
-                local info = UIDropDownMenu_CreateInfo()
-                info.text = type
-                info.value = i-1
-                info.func = endstopDropDownHandler
-                UIDropDownMenu_AddButton(info)
-            end
-            UIDropDownMenu_SetSelectedValue(endstopDropDown, JadeUIDB.endstopType) --This sets the label initially based on the SavedVariable
-        end
-
-        endstopDropDown:SetPoint("TOPLEFT", endstopLabel, "BOTTOMLEFT", -15, -5)
-        table.insert(JadeUI.OptionsListRight, endstopDropDown) --This frame has it's relative point set manually since it needs to be closer, and is added manually
-        UIDropDownMenu_SetWidth(endstopDropDown, 100)
-        endstopDropDown.tooltipText = "Select Endstop Artwork"
-        endstopDropDown.tooltipRequirement = "Select the artwork to be displayed for the endstops on the main bar"
-        UIDropDownMenu_Initialize(endstopDropDown, endstopDropDownInitialise)
-
-
-        --Register the Options Panel
-        optionsPanel.name = "JadeUI Classic" --CreateFrame call puts "Name" in the global scope, so _G["Name"] will retrieve the panel. The panel.name is a field on the panel itself.
+        --Register the Options Panel in the AddOn Menu
         InterfaceOptions_AddCategory(optionsPanel)
     end
 
